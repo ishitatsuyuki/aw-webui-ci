1dea59b827089007d9f64d557446b0ecc9bccf49
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2022-07-30 15:57:46 +0900

    pnpm test

--- src/util/awclient.ts
+++ src/util/awclient.ts
@@ -13,7 +13,7 @@ export function createClient(force?: boolean): AWClient {
   // Works since CORS is enabled by default when running `aw-server --testing`.
   if (!production) {
     const aw_server_url = typeof AW_SERVER_URL !== 'undefined' && AW_SERVER_URL;
-    baseURL = aw_server_url || 'http://127.0.0.1:5666';
+    baseURL = aw_server_url || 'http://127.0.0.1:5600';
   }
 
   if (!_client || force) {
